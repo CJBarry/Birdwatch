fb3458bf38d87e5a679ce46b567eaf1219f269d9
Ben <bluespider42@hotmail.co.uk>
2016-07-01 15:31:12 +0100

    added comment explanation

--- tmin_example.py
+++ tmin_example.py
@@ -1,4 +1,9 @@
 from met import met_hist
 
+# get_tmin(lat, lon, year, month)
 min_temp = met_hist.get_tmin(50.0, 1.1, 2015, 5)
+print(min_temp)
+
+# get_tmax(lat, lon, year, month)
+max_temp = met_hist.get_tmax(50.0, 1.1, 2015, 5)
 print(min_temp)
